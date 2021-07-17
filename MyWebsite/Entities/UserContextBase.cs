00a6575b94b212bd97aa6bae708ce5de1994b09d
nhannguyen2882 <62823878+nhannguyen2882@users.noreply.github.com>
2021-07-17 04:08:22 -0700

    first commit

---
+++ MyWebsite/Entities/UserContextBase.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyWebsite.Entities
+{
+    public class UserContextBase
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MyWebsite/Migrations/20210717105832_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyWebsite.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
